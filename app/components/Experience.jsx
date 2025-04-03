f780adc121b2908a0aa7677c2f8bce861518e182
grey <greys0@icloud.com>
2025-04-03 18:46:17 -0400

    Created Experience section.

---
+++ app/components/Experience.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <div id='about' className='w-full px-[12%] py-10 scroll-mt-20'>
+        <h2 className='text-center text-5xl'>My Experience</h2>
+    </div>
+  )
+}
+
+export default Experience
