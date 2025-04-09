b4011352a9bea23701b14a91964c9569217e1d49
grey <greys0@icloud.com>
2025-04-08 20:40:32 -0400

    Changed Experience page for Resume page.

--- app/page.js
+++ app/page.js
@@ -2,10 +2,10 @@
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
-import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 import Certificates from "./components/Certificates";
 import Projects from "./components/Projects";
+import Resume from "./components/Resume";
 
 export default function Home() {
   return (
@@ -13,7 +13,7 @@ export default function Home() {
         <Navbar />
         <Header />
         <About />
-        <Experience />
+        <Resume />
         <Projects />
         <Certificates />
         <Contact />
